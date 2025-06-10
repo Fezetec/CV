e37f96aafd660d0dd60477157361e39770fd3c70
Fezetec <vinjarkvamme@gmail.com>
2025-06-10 20:24:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
     const section = document.querySelector(link.getAttribute('href'));
-    section.scrollIntoView({ behavior: 'smooth' });
+    var headerOffset = 45;
+    var elementPosition = section.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    //section.scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({
+         top: offsetPosition,
+         behavior: "smooth"
+    });
   });
 });
